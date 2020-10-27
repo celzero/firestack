9d4a84259fbfc298fd7b5964605164f23e21b0df
Murtaza Aliakbar <murtaza@live.in>
2020-10-27 05:37:21 +0530

    Too many unknown conns on Android 9 and below
    On Android 9 and below, the app makes use of /proc/net to figure out owner
    uid given a 5-tuple. It turns out that often times v6 procfs ends up with
    entries for v4 conns (happy-eyeballs) but the app never accounted for those.
    
    Now, even when connections are v4, make an effort to check the v6 procfs
    entries to see if a v6-only network is tunneling v4 packets.
    
    Additionally, keep a cache of procfs entires for 30s or so to reduce the
    very costly scans on procfs.

--- tunnel/settings/procfs.go
+++ tunnel/settings/procfs.go
@@ -1,3 +1,4 @@
+// relicensed to apache v2 from opensnitch with permissions from evilsocket
 package settings
 
 import (
@@ -9,12 +10,15 @@ import (
 	"regexp"
 	"strconv"
 	"strings"
+	"sync"
+	"time"
 
 	"github.com/eycorsican/go-tun2socks/common/log"
 )
 
 const (
 	crlftabspace = "\r\n\t "
+	cachettl     = 30000 // millis
 )
 
 var (
@@ -39,6 +43,12 @@ var (
 		`(\d+)\s+` +
 		// the rest...
 		`.+`)
+
+	cache = NewProcNetCache()
+
+	zeroIP = net.ParseIP("::")
+
+	zeroPort = 0
 )
 
 // ProcNetEntry represents a single line as fetched from /proc/net/*
@@ -50,6 +60,20 @@ type ProcNetEntry struct {
 	DstPort  int
 	UserID   int
 	INode    int
+	ctime    time.Time
+}
+
+type ProcNetCache struct {
+	rw          sync.RWMutex
+	pool        map[string]*ProcNetEntry
+	lastcleanup time.Time
+}
+
+func NewProcNetCache() ProcNetCache {
+	return ProcNetCache{
+		pool:        map[string]*ProcNetEntry{},
+		lastcleanup: time.Now(),
+	}
 }
 
 // NewProcNetEntry creates an Entry
@@ -62,7 +86,37 @@ func NewProcNetEntry(protocol string, srcIP net.IP, srcPort int, dstIP net.IP, d
 		DstPort:  dstPort,
 		UserID:   userID,
 		INode:    iNode,
+		ctime:    time.Now(),
 	}
+}
+
+func (p *ProcNetEntry) String() string {
+	return p.Protocol + p.SrcIP.String() + strconv.Itoa(p.SrcPort) + p.DstIP.String() + strconv.Itoa(p.DstPort)
+}
+
+func (p *ProcNetEntry) Same(q *ProcNetEntry) bool {
+	if p == nil || q == nil {
+		return false
+	}
+
+	// https://github.com/M66B/NetGuard/blob/1fe3a04ae/app/src/main/jni/netguard/ip.c#L393
+	skipSrcIP := false
+	skipDstIP := false
+	skipDstPort := false
+	if zeroIP.Equal(p.SrcIP) || zeroIP.Equal(q.SrcIP) {
+		skipSrcIP = true
+	}
+	if zeroIP.Equal(p.DstIP) || zeroIP.Equal(q.DstIP) {
+		skipDstIP = true
+	}
+	if zeroPort == p.DstPort || zeroPort == q.DstPort {
+		skipDstPort = true
+	}
+
+	return (skipSrcIP || p.SrcIP.Equal(q.SrcIP)) &&
+		p.SrcPort == q.SrcPort &&
+		(skipDstIP || p.DstIP.Equal(q.DstIP)) &&
+		(skipDstPort || p.DstPort == q.DstPort)
 }
 
 func trim(s string) string {
@@ -85,23 +139,23 @@ func hexToInt(h string) int {
 	return int(d)
 }
 
-func hexToInt2(h string) (int, int) {
+func hexToInt2(h string) (uint, uint) {
 	if len(h) > 16 {
-		d, err := strconv.ParseInt(h[:16], 16, 64)
+		d, err := strconv.ParseUint(h[:16], 16, 64)
+		if err != nil {
+			log.Errorf("Error while parsing %s to int: %s", h[:16], err)
+		}
+		d2, err := strconv.ParseUint(h[16:], 16, 64)
 		if err != nil {
 			log.Errorf("Error while parsing %s to int: %s", h[16:], err)
 		}
-		d2, err := strconv.ParseInt(h[16:], 16, 64)
-		if err != nil {
-			log.Errorf("Error while parsing %s to int: %s", h[16:], err)
-		}
-		return int(d), int(d2)
+		return uint(d), uint(d2)
 	}
-	d, err := strconv.ParseInt(h, 16, 64)
+	d, err := strconv.ParseUint(h, 16, 64)
 	if err != nil {
-		log.Errorf("Error while parsing %s to int: %s", h[16:], err)
+		log.Errorf("Error while parsing %s to int: %s", h[:16], err)
 	}
-	return int(d), 0
+	return uint(d), 0
 
 }
 
@@ -109,11 +163,24 @@ func hexToIP(h string) net.IP {
 	n, m := hexToInt2(h)
 	var ip net.IP
 	if m != 0 {
+		mmsb := uint32(m >> 32)
+		v4IPv6 := false
 		ip = make(net.IP, 16)
+
+		// https://stackoverflow.com/questions/22751035
+		if n == 0 && mmsb == 0 {
+			v4IPv6 = true // ipv4 in ipv6
+		}
 		// TODO: Check if this depends on machine endianness?
 		binary.LittleEndian.PutUint32(ip, uint32(n>>32))
 		binary.LittleEndian.PutUint32(ip[4:], uint32(n))
-		binary.LittleEndian.PutUint32(ip[8:], uint32(m>>32))
+		if v4IPv6 {
+			// https://github.com/golang/go/blob/2bed2797/src/net/ip.go#L195-L196
+			// 0000 0000 0000 0000 1111 1111 1111 1111
+			binary.BigEndian.PutUint32(ip[8:], uint32(0xffff))
+		} else {
+			binary.LittleEndian.PutUint32(ip[8:], mmsb)
+		}
 		binary.LittleEndian.PutUint32(ip[12:], uint32(m))
 	} else {
 		ip = make(net.IP, 4)
@@ -157,12 +224,93 @@ func ParseProcNet(protocol string) ([]ProcNetEntry, error) {
 		))
 	}
 
+	go cleanupPool()
+
 	return entries, nil
 }
 
-// FindProcNetEntry parses /proc/net/* and return the line matching the argument five-tuple
+func cleanupPool() {
+	// golang locks aren't re-entrant, careful java developers
+	cache.rw.Lock()
+	if time.Since(cache.lastcleanup).Milliseconds() <= cachettl {
+		cache.rw.Unlock()
+		return
+	}
+	cache.lastcleanup = time.Now()
+	cache.rw.Unlock()
+
+	for _, v := range cache.pool {
+		// slight window of opportunity for a data-race when
+		// invalidProcNetEntry says one thing but the state
+		// has changed by the time delete does its thing
+		if invalidProcNetEntry(v) {
+			// delete does its own locking
+			deleteProcNetEntryFromPool(v)
+		}
+	}
+}
+
+func invalidProcNetEntry(p *ProcNetEntry) bool {
+	if p == nil {
+		return true
+	}
+
+	cache.rw.RLock()
+	defer cache.rw.RUnlock()
+
+	e := cache.pool[p.String()]
+
+	return e == nil || time.Since(e.ctime).Milliseconds() > cachettl
+}
+
+func deleteProcNetEntryFromPool(p *ProcNetEntry) {
+	if p == nil {
+		return
+	}
+
+	cache.rw.Lock()
+	defer cache.rw.Unlock()
+
+	delete(cache.pool, p.String())
+}
+
+func addProcNetEntryToPool(p *ProcNetEntry) {
+	if p == nil {
+		return
+	}
+
+	cache.rw.Lock()
+	defer cache.rw.Unlock()
+
+	cache.pool[p.String()] = p
+}
+
+func getProcNetEntryFromPool(p *ProcNetEntry) *ProcNetEntry {
+	if p == nil {
+		return nil
+	}
+
+	cache.rw.RLock()
+	defer cache.rw.RUnlock()
+
+	return cache.pool[p.String()]
+}
+
+// findProcNetEntryForProtocol parses /proc/net/* and return the line matching the argument five-tuple
 // (protocol, source, sport, destination, dport) as NewProcNetEntry.
-func FindProcNetEntry(protocol string, srcIP net.IP, srcPort int, dstIP net.IP, dstPort int) *ProcNetEntry {
+func findProcNetEntryForProtocol(protocol string, srcIP net.IP, srcPort int, dstIP net.IP, dstPort int) *ProcNetEntry {
+
+	n := NewProcNetEntry(protocol, srcIP, srcPort, dstIP, dstPort, 0, 0)
+	e := &n // https://groups.google.com/g/golang-nuts/c/reaIlFdibWU?pli=1
+	pool := getProcNetEntryFromPool(e)
+
+	if e.Same(pool) {
+		if invalidProcNetEntry(pool) == false {
+			return pool
+		}
+		deleteProcNetEntryFromPool(pool)
+	}
+
 	entries, err := ParseProcNet(protocol)
 	if err != nil {
 		log.Warnf("Error while searching for %s netstat entry: %s", protocol, err)
@@ -170,9 +318,28 @@ func FindProcNetEntry(protocol string, srcIP net.IP, srcPort int, dstIP net.IP, 
 	}
 
 	for _, entry := range entries {
-		if srcIP.Equal(entry.SrcIP) && srcPort == entry.SrcPort && dstIP.Equal(entry.DstIP) && dstPort == entry.DstPort {
+		pe := getProcNetEntryFromPool(&entry)
+		if invalidProcNetEntry(pe) {
+			addProcNetEntryToPool(&entry)
+		}
+		if e.Same(&entry) {
 			return &entry
 		}
+	}
+
+	return nil
+}
+
+// FindProcNetEntry searches for netstat entries in v4 and v6 tables.
+func FindProcNetEntry(protocol string, srcIP net.IP, srcPort int, dstIP net.IP, dstPort int) *ProcNetEntry {
+	if entry := findProcNetEntryForProtocol(protocol, srcIP, srcPort, dstIP, dstPort); entry != nil {
+		return entry
+	}
+
+	ipv6Suffix := "6"
+	if strings.HasSuffix(protocol, ipv6Suffix) == false {
+		otherProtocol := protocol + ipv6Suffix
+		return findProcNetEntryForProtocol(otherProtocol, srcIP, srcPort, dstIP, dstPort)
 	}
 
 	return nil
