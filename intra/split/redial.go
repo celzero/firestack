7e576057183cf9995a89f1ef7ddb434b58186ff7
Murtaza Aliakbar <murtaza@live.in>
2023-10-04 05:12:09 +0530

    split/redial: support udp and non-split dialing

--- intra/split/redial.go
+++ intra/split/redial.go
@@ -17,10 +17,19 @@ import (
 	"github.com/celzero/firestack/intra/protect/ipmap"
 )
 
+type establishConnFunc func(*protect.RDial, string, netip.Addr, int) (net.Conn, error)
+
 var ipm ipmap.IPMap = ipmap.NewIPMap()
 
+func addr(ip netip.Addr, port int) string {
+	return net.JoinHostPort(ip.String(), strconv.Itoa(port))
+}
 func tcpaddr(ip netip.Addr, port int) *net.TCPAddr {
 	return &net.TCPAddr{IP: ip.AsSlice(), Port: port}
+}
+
+func udpaddr(ip netip.Addr, port int) *net.UDPAddr {
+	return &net.UDPAddr{IP: ip.AsSlice(), Port: port}
 }
 
 func Renew(hostname string, addrs []string) bool {
@@ -58,30 +67,62 @@ func Disconfirm(hostname string, ip net.Addr) bool {
 	return false
 }
 
-func ReDial(dialer *protect.RDial, network, addr string) (net.Conn, error) {
-	start := time.Now()
-	log.D("redial: dialing %s", addr)
-	domain, portStr, err := net.SplitHostPort(addr)
-	if err != nil {
-		return nil, err
+func dial(d *protect.RDial, proto string, ip netip.Addr, port int) (net.Conn, error) {
+	switch proto {
+	case "tcp", "tcp4", "tcp6":
+		return d.DialTCP(proto, nil, tcpaddr(ip, port))
+	case "udp", "udp4", "udp6":
+		return d.DialUDP(proto, nil, udpaddr(ip, port))
+	default:
+		return d.Dial(proto, addr(ip, port))
 	}
-	port, err := strconv.Atoi(portStr)
-	if err != nil {
-		return nil, err
-	}
+}
 
+func splitdial(d *protect.RDial, proto string, ip netip.Addr, port int) (net.Conn, error) {
+	switch proto {
+	case "tcp", "tcp4", "tcp6":
+		if conn, err := DialWithSplitRetry(d, tcpaddr(ip, port)); err == nil {
+			log.D("redial: tcp: confirmed IP %s worked for %s", ip)
+			return conn, nil
+		}
+	case "udp", "udp4", "udp6":
+		if conn, err := d.DialUDP(proto, nil, udpaddr(ip, port)); err == nil {
+			log.D("redial: udp: confirmed IP %s worked for %s", ip)
+			return conn, nil
+		}
+	default:
+		log.I("redial: unknown network %s", proto)
+		return d.Dial(proto, addr(ip, port))
+	}
+	return nil, net.UnknownNetworkError(proto)
+}
+
+func Dial(d *protect.RDial, network, addr string) (net.Conn, error) {
+	return commondial(d, network, addr, dial)
+}
+
+func commondial(d *protect.RDial, network, addr string, connect establishConnFunc) (net.Conn, error) {
+	start := time.Now()
+
+	log.D("commondial: dialing %s", addr)
+	domain, portstr, err := net.SplitHostPort(addr)
+	if err != nil {
+		return nil, err
+	}
+	port, err := strconv.Atoi(portstr)
+	if err != nil {
+		return nil, err
+	}
 	// TODO: Improve IP fallback strategy with parallelism and Happy Eyeballs.
 	var conn net.Conn
 	ips := ipm.Get(domain)
 	confirmed := ips.Confirmed()
 	if confirmed.IsValid() {
-		log.D("redial: trying IP %s for addr %s", confirmed, addr)
-		if conn, err = DialWithSplitRetry(dialer, tcpaddr(confirmed, port)); err == nil {
-			log.I("redial: confirmed IP %s worked for %s", confirmed, addr)
+		if conn, err := connect(d, network, confirmed, port); err == nil {
 			return conn, nil
 		}
-		go ips.Disconfirm(confirmed)
-		log.D("redial: IP %s for %s failed with err %v", confirmed, addr, err)
+		ips.Disconfirm(confirmed)
+		log.D("commondial: confirmed IP %s for %s failed with err %v", confirmed, addr, err)
 	}
 
 	allips := ips.GetAll()
@@ -91,15 +132,21 @@ func ReDial(dialer *protect.RDial, network, addr string) (net.Conn, error) {
 		if ip.Compare(confirmed) == 0 || !ip.IsValid() {
 			continue
 		}
-		if conn, err = DialWithSplitRetry(dialer, tcpaddr(ip, port)); err == nil {
-			go ips.Confirm(ip)
-			log.I("redial: found working IP %s for %s", ip, addr)
+		if conn, err = connect(d, network, ip, port); err == nil {
+			ips.Confirm(ip)
+			log.I("commondial: found working IP %s for %s", ip, addr)
 			return conn, nil
 		}
 	}
 
-	log.W("redial: dur: %ss; renew %s", time.Since(start).Seconds(), addr)
+	dur := time.Since(start).Seconds()
+	log.W("commondial: duration: %ss; renew %s", dur, addr)
+
 	go Renew(domain, ips.Seed())
 
-	return dialer.Dial(network, addr)
+	return d.Dial(network, addr)
+}
+
+func ReDial(d *protect.RDial, network, addr string) (net.Conn, error) {
+	return commondial(d, network, addr, splitdial)
 }
