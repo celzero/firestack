edb7d573458988b84cbf1aa567afab5279ec86ee
Murtaza Aliakbar <murtaza@live.in>
2024-06-28 22:03:28 +0530

    tcp,udp: m fmt

--- intra/common.go
+++ intra/common.go
@@ -304,7 +304,7 @@ type zeroListener struct{}
 
 var _ SocketListener = (*zeroListener)(nil)
 
-func (*zeroListener) OnSocketClosed(*SocketSummary)                  {}
 func (*zeroListener) Flow(_, _ int32, _, _, _, _, _, _ string) *Mark { return nil }
+func (*zeroListener) OnSocketClosed(*SocketSummary)                  {}
 
 var nooplistener = new(zeroListener)
