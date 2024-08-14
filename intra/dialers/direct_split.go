d4be97fb94f1d1f0f131959227317cea50ac6661
Murtaza Aliakbar <murtaza@live.in>
2024-08-15 03:19:09 +0530

    dialers/split: unembed TCPConn from splitter struct

--- intra/dialers/direct_split.go
+++ intra/dialers/direct_split.go
@@ -18,15 +18,17 @@ import (
 	"io"
 	"net"
 	"sync/atomic"
+	"time"
 
 	"github.com/celzero/firestack/intra/core"
+	"github.com/celzero/firestack/intra/log"
 	"github.com/celzero/firestack/intra/protect"
 	"github.com/celzero/firestack/intra/settings"
 )
 
 type splitter struct {
-	*net.TCPConn
-	strat int32
+	conn  *net.TCPConn
+	strat int32       // settings.Split* constant
 	used  atomic.Bool // Initially false.  Becomes true after the first write.
 }
 
@@ -35,19 +37,33 @@ var _ core.DuplexConn = (*splitter)(nil)
 // DialWithSplit returns a TCP connection that always splits the initial upstream segment.
 // Like net.Conn, it is intended for two-threaded use, with one thread calling
 // Read and CloseRead, and another calling Write, ReadFrom, and CloseWrite.
-func DialWithSplit(d *protect.RDial, addr *net.TCPAddr) (*splitter, error) {
+func DialWithSplit(d *protect.RDial, addr *net.TCPAddr) (core.DuplexConn, error) {
+	ds := settings.GetDialerOpts()
+	return dialWithSplitStrat(ds.Strat, d, addr)
+}
+
+// dialWithSplitStrat returns a TCP connection that always splits the initial upstream segment
+// using the specified strategy, strat, which is one of the settings.Split* constants.
+func dialWithSplitStrat(dialStrat int32, d *protect.RDial, addr *net.TCPAddr) (core.DuplexConn, error) {
+	switch dialStrat {
+	case settings.SplitDesync:
+		return dialWithSplitAndDesync(d, addr.AddrPort())
+	case settings.SplitTCP, settings.SplitTCPOrTLS:
+		fallthrough
+	default:
+	}
 	conn, err := d.DialTCP(addr.Network(), nil, addr)
 	if err != nil {
 		return nil, err
 	}
 	if conn == nil {
-		return nil, net.UnknownNetworkError("no conn")
+		return nil, errNoConn
 	}
-	strat := settings.DialStrategy.Load()
-	return &splitter{TCPConn: conn, strat: strat}, nil
+	// todo: strat must be tcp or tls
+	return &splitter{conn: conn, strat: dialStrat}, nil
 }
 
-// Write-related functions
+// Write implements DuplexConn.
 func (s *splitter) Write(b []byte) (n int, err error) {
 	conn := s.TCPConn
 	strat := s.strat
@@ -75,7 +91,48 @@ func (s *splitter) ReadFrom(reader io.Reader) (bytes int64, err error) {
 	}
 
 	var b int64
-	b, err = s.TCPConn.ReadFrom(reader)
+	b, err = s.conn.ReadFrom(reader)
 	bytes += b
 	return
 }
+
+// Read implements DuplexConn.
+func (s *splitter) Read(b []byte) (int, error) { return s.conn.Read(b) }
+
+// LocalAddr implements DuplexConn.
+func (s *splitter) LocalAddr() net.Addr { return laddr(s.conn) }
+
+// RemoteAddr implements DuplexConn.
+func (s *splitter) RemoteAddr() net.Addr { return raddr(s.conn) }
+
+func (s *splitter) SetDeadline(t time.Time) error {
+	if c := s.conn; c != nil {
+		return c.SetDeadline(t)
+	}
+	return nil // no-op
+}
+
+// SetReadDeadline implements DuplexConn.
+func (s *splitter) SetReadDeadline(t time.Time) error {
+	if c := s.conn; c != nil {
+		return c.SetReadDeadline(t)
+	}
+	return nil // no-op
+}
+
+// SetWriteDeadline implements DuplexConn.
+func (s *splitter) SetWriteDeadline(t time.Time) error {
+	if c := s.conn; c != nil {
+		return c.SetWriteDeadline(t)
+	}
+	return nil // no-op
+}
+
+// Close implements DuplexConn.
+func (s *splitter) Close() error { core.CloseTCP(s.conn); return nil }
+
+// CloseRead implements DuplexConn.
+func (s *splitter) CloseRead() error { core.CloseTCPRead(s.conn); return nil }
+
+// CloseWrite implements DuplexConn.
+func (s *splitter) CloseWrite() error { core.CloseTCPWrite(s.conn); return nil }
