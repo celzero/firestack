4c0fd4a275e3c76d25c189523fc3f580385f4fdc
Murtaza Aliakbar <murtaza@live.in>
2024-10-15 05:11:18 +0530

    ipn/proxy: TestWarp over base not exit

--- intra/ipn/proxies.go
+++ intra/ipn/proxies.go
@@ -547,8 +547,8 @@ func (px *proxifier) TestWarp() (string, error) {
 
 			var c4, c6 protect.Conn
 			var err4, err6 error
-			c4, err4 = px.exit.Dial("udp", v4.String())
-			c6, err6 = px.exit.Dial("udp", v6.String())
+			c4, err4 = px.base.Dial("udp", v4.String())
+			c6, err6 = px.base.Dial("udp", v6.String())
 			defer core.CloseConn(c4, c6)
 
 			// net.OpError => os.SyscallError => syscall.Errno
