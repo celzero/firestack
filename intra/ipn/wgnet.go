402fb38c5e8b8984e56b90991d5b103df26ecf6d
Murtaza Aliakbar <murtaza@live.in>
2025-03-22 06:06:34 +0530

    lint: ipn/wg.go

--- intra/ipn/wgnet.go
+++ intra/ipn/wgnet.go
@@ -76,7 +76,9 @@ func (tnet *wgtun) LookupContextHost(ctx context.Context, host string) ([]netip.
 
 	// dialers.Resolve returns from cache (which may be stale)
 	if ips, err := dialers.Resolve(host, tnet.ID()); len(ips) <= 0 {
-		err = core.JoinErr(err, errNoSuchHost)
+		if err == nil {
+			err = errNoSuchHost
+		}
 		log.D("wg: %s dial: lookup failed %q: no ips; err: %v", tnet.id, host, err)
 		return nil, &net.DNSError{Err: err.Error(), Name: host, IsNotFound: true}
 	} else {
