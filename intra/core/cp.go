9aba11afaf9a427a7f41fe189371a4c6f7ce04d2
Murtaza Aliakbar <murtaza@live.in>
2025-05-19 23:02:23 +0530

    core/cp: m attribution

--- intra/core/cp.go
+++ intra/core/cp.go
@@ -56,6 +56,7 @@ func Stream(dst io.Writer, src io.Reader) (int64, error) {
 	)
 }
 
+// ref: github.com/golang/go/issues/58808
 // from: go-review.googlesource.com/c/go/+/472475/20/src/net/net.go
 
 // noReadFrom can be embedded alongside another type to
