0fb61e7d6d3281e633f41ee0b7ecb224e7fe9d6d
Murtaza Aliakbar <murtaza@live.in>
2025-06-03 22:11:00 +0530

    core/weakref: pky unique too heavy
    revert to commit: 6ecf9470872f

--- intra/core/weakref.go
+++ intra/core/weakref.go
@@ -8,7 +8,8 @@ package core
 
 import (
 	"errors"
-	"unique"
+	"sync"
+	"weak"
 )
 
 var errNoCreat = errors.New("weak: create fn nil")
@@ -18,10 +19,9 @@ type reftest[V any] func(*V) bool
 
 func refpass[V any](_ *V) bool { return true }
 
-// WeakRef is a weak reference to a value of type V.
 type WeakRef[V any] struct {
-	// unique.Handle holds a weak reference to *V.
-	weak  unique.Handle[*V]
+	mu    sync.RWMutex
+	weak  weak.Pointer[V]
 	creat reffactory[V]
 	test  reftest[V]
 }
@@ -35,22 +35,25 @@ func NewWeakRef[V any](creat reffactory[V], test reftest[V]) (*WeakRef[V], error
 	}
 
 	return &WeakRef[V]{
-		weak:  unique.Make[*V](nil), // initially nil
 		creat: creat,
 		test:  test,
 	}, nil
 }
 
 func (w *WeakRef[V]) load() (v *V, valid bool) {
+	defer func() { // test without lock held
+		valid = v != nil && w.test(v)
+	}()
+
+	w.mu.RLock()
+	defer w.mu.RUnlock()
 	v = w.weak.Value()
-	valid = v != nil && w.test(v)
 	return
 }
 
-func (w *WeakRef[V]) store() (v *V, valid bool) {
+func (w *WeakRef[V]) storeLocked() (v *V) {
 	v = w.creat()
-	w.weak = unique.Make(v)
-	valid = v != nil && w.test(v)
+	w.weak = weak.Make(v)
 	return
 }
 
@@ -58,7 +61,17 @@ func (w *WeakRef[V]) loadOrStore() (v *V, valid bool) {
 	if v, valid = w.load(); valid {
 		return
 	}
-	return w.store() // new v
+
+	defer func() { // test without lock held
+		valid = v != nil && w.test(v)
+	}()
+
+	w.mu.Lock()
+	defer w.mu.Unlock()
+	if v = w.weak.Value(); v == nil { // gc won
+		v = w.storeLocked() // new v
+	} // else: use existing v
+	return
 }
 
 func (w *WeakRef[V]) Ref() (v *V, valid bool) {
