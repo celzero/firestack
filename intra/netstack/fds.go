3cade9227986fdc8397570c8067d76c10f9916ba
Murtaza Aliakbar <murtaza@live.in>
2025-07-31 20:46:07 +0530

    netstack: prevent stop() on invalid fd
    If stop() is called twice (which it shouldn't be but it somehow is), the
    2nd stop() call would invariably act on an "invalidfd" (currently set to -1).
    The code today is setup to panic when that happens. Instead change it to
    an error so that the runtime does not crash.
    
    Calling stop() on an invalidfd shouldn't happen unless firestack is in Loopback
    mode, which isn't enabled by default anyway. That is, in Loopback, the client
    code is required to call tunnel.Dispose before tunnel.SetLink. tunnel.Dispose
    stop()s the current fd and sets the new fd to "invalidfd" until the next
    tunnel.SetLink call sets a proper (tunnel) fd.

--- intra/netstack/fds.go
+++ intra/netstack/fds.go
@@ -32,7 +32,7 @@ import (
 	"golang.org/x/sys/unix"
 )
 
-var invalidFds = &fds{stopFd{efd: -1}, invalidfd}
+var invalidFds = &fds{stopFd{efd: invalidfd}, invalidfd}
 
 // stopFd is an eventfd used to signal the stop of a dispatcher.
 type stopFd struct {
@@ -49,15 +49,19 @@ func newStopFd() (stopFd, error) {
 
 // stop writes to the eventfd and notifies the dispatcher to stop. It does not
 // block.
-func (s *stopFd) stop() {
+func (s *stopFd) stop() error {
+	if s.efd == invalidfd || s.efd <= 2 {
+		return nil
+	}
 	increment := []byte{1, 0, 0, 0, 0, 0, 0, 0}
 	if n, err := unix.Write(s.efd, increment); n != len(increment) || err != nil {
 		// There are two possible errors documented in eventfd(2) for writing:
 		// 1. We are writing 8 bytes and not 0xffffffffffffff, thus no EINVAL.
 		// 2. stop is only supposed to be called once, it can't reach the limit,
 		// thus no EAGAIN.
-		panic(fmt.Sprintf("write(efd) = (%d, %s), want (%d, nil)", n, err, len(increment)))
+		return fmt.Errorf("write(efd) = (%d, %s), want (%d, nil)", n, err, len(increment))
 	}
+	return nil
 }
 
 type fds struct {
@@ -66,6 +70,7 @@ type fds struct {
 }
 
 // Takes ownership of fd, which must be a valid TUN file descriptor.
+// Never returns nil, but may return an invalid fds (with tunFd = -1).
 func newTun(fd int) (*fds, error) {
 	if fd == invalidfd {
 		return invalidFds, nil
@@ -73,12 +78,12 @@ func newTun(fd int) (*fds, error) {
 	err := unix.SetNonblock(fd, true)
 	if err != nil {
 		clos(fd)
-		return nil, err
+		return invalidFds, err
 	}
 	stopFd, err := newStopFd()
 	if err != nil {
 		clos(fd)
-		return nil, err
+		return invalidFds, err
 	}
 	return &fds{stopFd, fd}, nil
 }
@@ -88,14 +93,14 @@ func (f *fds) ok() bool {
 }
 
 func (f *fds) eve() int {
-	if f != nil {
+	if f != nil && f.stopFd.efd > 2 {
 		return f.stopFd.efd
 	}
 	return invalidfd
 }
 
 func (f *fds) tun() int {
-	if f != nil {
+	if f != nil && f.tunFd > 2 {
 		return f.tunFd
 	}
 	return invalidfd
@@ -103,10 +108,10 @@ func (f *fds) tun() int {
 
 func (f *fds) stop() {
 	if f.ok() {
-		f.stopFd.stop()
-		err := syscall.Close(f.tunFd)
-		log.I("ns: dispatch: fds: stop: eve(%d) tun(%d); err? %v",
-			f.stopFd.efd, f.tunFd, err)
+		err1 := f.stopFd.stop()
+		err2 := syscall.Close(f.tunFd)
+		logeif(err1)("ns: dispatch: fds: stop: eve(%d) tun(%d); errs? %v %v",
+			f.stopFd.efd, f.tunFd, err1, err2)
 	} else {
 		log.W("ns: dispatch: fds: stop: no-op")
 	}
