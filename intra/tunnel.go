dd4690a5f0b848c50be17b50435d7bd55d8da5ce
Murtaza Aliakbar <murtaza@live.in>
2025-08-08 23:18:25 +0530

    intra/tunnel: send to Console if Bridge non-nil

--- intra/tunnel.go
+++ intra/tunnel.go
@@ -436,5 +436,7 @@ type clogAdapter struct {
 var _ log.Console = (*clogAdapter)(nil)
 
 func (l *clogAdapter) Log(lvl log.LogLevel, msg log.Logmsg) {
-	l.b.Log(int32(lvl), x.StrOf(msg)) // adopt the log message
+	if bdg := l.b; bdg != nil {
+		bdg.Log(int32(lvl), x.StrOf(msg)) // adopt the log message
+	}
 }
