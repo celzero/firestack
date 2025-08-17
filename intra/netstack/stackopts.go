de707d18fffd44c2ad455fe01549c2a8e3e95221
Murtaza Aliakbar <murtaza@live.in>
2025-08-18 03:42:16 +0530

    netstack/sockopts: disable unless enabled

--- intra/netstack/stackopts.go
+++ intra/netstack/stackopts.go
@@ -19,25 +19,25 @@ func SetNetstackOpts(s *stack.Stack) {
 	// TODO: other stack otps?
 	// github.com/xjasonlyu/tun2socks/blob/31468620e/core/option/option.go#L69
 
-	// TODO: setup protocol opts?
-	// github.com/google/gvisor/blob/ef9e8d91/test/benchmarks/tcp/tcp_proxy.go#L233
-	sack := tcpip.TCPSACKEnabled(true)
-	_ = s.SetTransportProtocolOption(tcp.ProtocolNumber, &sack)
+	if settings.GetDialerOpts().LowerKeepAlive {
+		// TODO: setup protocol opts?
+		// github.com/google/gvisor/blob/ef9e8d91/test/benchmarks/tcp/tcp_proxy.go#L233
+		sack := tcpip.TCPSACKEnabled(true)
+		_ = s.SetTransportProtocolOption(tcp.ProtocolNumber, &sack)
 
-	// from: github.com/telepresenceio/telepresence/blob/ab7dda7d55/pkg/vif/stack.go#L232
-	// Enable Receive Buffer Auto-Tuning, see: github.com/google/gvisor/issues/1666
-	bufauto := tcpip.TCPModerateReceiveBufferOption(true)
-	_ = s.SetTransportProtocolOption(tcp.ProtocolNumber, &bufauto)
+		// from: github.com/telepresenceio/telepresence/blob/ab7dda7d55/pkg/vif/stack.go#L232
+		// Enable Receive Buffer Auto-Tuning, see: github.com/google/gvisor/issues/1666
+		bufauto := tcpip.TCPModerateReceiveBufferOption(true)
+		_ = s.SetTransportProtocolOption(tcp.ProtocolNumber, &bufauto)
 
-	// coder.com/blog/delivering-5x-faster-throughput-in-coder-2-12-0
-	ccopt := tcpip.CongestionControlOption("cubic")
-	_ = s.SetTransportProtocolOption(tcp.ProtocolNumber, &ccopt)
+		// coder.com/blog/delivering-5x-faster-throughput-in-coder-2-12-0
+		ccopt := tcpip.CongestionControlOption("cubic")
+		_ = s.SetTransportProtocolOption(tcp.ProtocolNumber, &ccopt)
 
-	ttl := tcpip.DefaultTTLOption(128)
-	s.SetNetworkProtocolOption(ipv4.ProtocolNumber, &ttl)
-	s.SetNetworkProtocolOption(ipv6.ProtocolNumber, &ttl)
+		ttl := tcpip.DefaultTTLOption(128)
+		s.SetNetworkProtocolOption(ipv4.ProtocolNumber, &ttl)
+		s.SetNetworkProtocolOption(ipv6.ProtocolNumber, &ttl)
 
-	if settings.ExperimentalWireGuard.Load() {
 		// github.com/tailscale/tailscale/blob/c4d0237e5c/wgengine/netstack/netstack_tcpbuf_default.go
 		tcpRXBufOpt := tcpip.TCPReceiveBufferSizeRangeOption{
 			Min:     tcp.MinBufferSize,
