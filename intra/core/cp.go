e836fe1874824a85d615b97983761b1a944252c2
Murtaza Aliakbar <murtaza@live.in>
2025-09-25 19:32:57 +0530

    core/cp: streaming case for retrier conn

--- intra/core/cp.go
+++ intra/core/cp.go
@@ -27,6 +27,14 @@ func Pipe(dst io.Writer, src io.Reader) (int64, error) {
 		return 0, errNoPipe
 	}
 
+	// Retrier conns have specific entry-points; make sure those
+	// get priority over regular copy.
+	if x, ok := src.(WriteRetrierConn); ok {
+		x.WriteTo(dst)
+	} else if x, ok := dst.(ReadRetrierConn); ok {
+		x.ReadFrom(src)
+	}
+
 	// Prefer WriteTo/ReadFrom if available as they are zero-copy.
 	// also: github.com/acln0/zerocopy
 	if x, ok := src.(io.WriterTo); ok {
