db37061da6b36c0420b75003eacc57fe773607d9
Murtaza Aliakbar <murtaza@live.in>
2026-05-27 20:35:52 +0530

    tcp,udp: m relabel conntrack

--- intra/common.go
+++ intra/common.go
@@ -95,8 +95,8 @@ func newBaseHandler(pctx context.Context, proto string, r dnsx.Resolver, px ipn.
 		prox:        px,
 		smmch:       make(chan *SocketSummary, smmchSize),
 		listener:    l,
-		fwtracker:   core.NewExpiringMap[string, string](pctx, "c.fwtrack"),
-		conntracker: core.NewConnMap("c.conntrack"),
+		fwtracker:   core.NewExpiringMap[string, string](pctx, proto+".fwtrack"),
+		conntracker: core.NewConnMap(proto + ".conntrack"),
 		status:      core.NewVolatile(HDLOK),
 	}
 	context.AfterFunc(pctx, h.End)
